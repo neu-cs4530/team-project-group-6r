075e5395e809598c8cbe5c79f298cbe5eff49248
Bob Joe <vzhan@Bobs-MacBook-Pro.local>
2022-03-24 16:26:00 -0400

    connection to MongoDB

--- services/townService/src/server.ts
+++ services/townService/src/server.ts
@@ -1,6 +1,7 @@
 import Express from 'express';
 import * as http from 'http';
 import CORS from 'cors';
+import mongoose from 'mongoose';
 import { AddressInfo } from 'net';
 import addTownRoutes from './router/towns';
 import CoveyTownsStore from './lib/CoveyTownsStore';
@@ -9,7 +10,12 @@ const app = Express();
 app.use(CORS());
 const server = http.createServer(app);
 
-addTownRoutes(server, app);
+
+const uri = 'mongodb+srv://Vevey:User1@coveytown.kt2xq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(uri).then(() => { console.log('MongoDB Connected') }).catch(err => console.log(err));
+
+
+addTownRoutes(server, app)
 
 server.listen(process.env.PORT || 8081, () => {
   const address = server.address() as AddressInfo;
