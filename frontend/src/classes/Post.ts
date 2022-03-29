bcf232a3e56920006aba979abbb336f07b3f91ca
Bob Joe <vzhan@Bobs-MacBook-Pro.local>
2022-03-29 19:10:10 -0400

    d

---
+++ frontend/src/classes/Post.ts
@@ -0,0 +1,5 @@
+export type ServerPost = {};
+
+export default class Post {
+    
+}
--- frontend/src/classes/TownsServiceClient.ts
+++ frontend/src/classes/TownsServiceClient.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import assert from 'assert';
 import { ServerPlayer } from './Player';
 import { ServerConversationArea } from './ConversationArea';
+import { ServerPost } from './Post';
 
 /**
  * The format of a request to join a Town in Covey.Town, as dispatched by the server middleware
@@ -34,6 +35,8 @@ export interface TownJoinResponse {
   isPubliclyListed: boolean;
   /** Names and occupants of any existing ConversationAreas */
   conversationAreas: ServerConversationArea[];
+  /** List of posts currently in this town */
+  posts: ServerPost[];
 }
 
 /**
