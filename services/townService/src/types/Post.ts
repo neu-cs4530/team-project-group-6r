4a351f37b097e729805ff26bd29f79ec3c764132
Cheng Xi Tsou <Kewlwasabi@gmail.com>
2022-03-29 12:09:28 -0400

    Post Schema

--- services/townService/src/types/Post.ts
+++ services/townService/src/types/Post.ts
@@ -1,10 +1,24 @@
 import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
-    postID: {
-        type: String
-    },
+    postID: String,
+    title: String,
+    postContent: String,
+    ownerID: String,
+    isVisible: Boolean,
+    postTime: Date,
+    updateTime: Date,
+    expirationTime: Date,
+    coordinates: {
+        x: Number,
+        y: Number 
+    }
+
 });
+
+PostSchema.add({
+    posts: [PostSchema]
+})
 
 const Post = mongoose.model("Post", PostSchema);
 
