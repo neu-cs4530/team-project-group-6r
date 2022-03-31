baffde3bd4a4d12560db6801932c6b461ea89c82
chy101010 <jason2028823@gmail.com>
2022-03-31 14:59:18 -0400

    init

---
+++ services/townService/src/lib/PostCoveyTownController.ts
@@ -0,0 +1,45 @@
+import PlayerSession from '../types/PlayerSession';
+import Player from '../types/Player';
+import CoveyTownController from './CoveyTownController';
+import PostController from './PostController';
+import Post from '../types/Post';
+import Comment from '../types/Comment';
+
+export default class PostCoveyTownController extends CoveyTownController {
+    // Owner
+    private _owner: string;
+    // Administrators
+    private _administrators: string[];
+    // Post Controller
+    private _postController: PostController;
+
+    async addPost(session: PlayerSession, post: Post): Promise<boolean> {
+        // Area collision?
+    }
+
+    async addComment(session: PlayerSession, post: Post, comment: Comment): Promise<boolean> {
+
+    }
+
+    async hidePost(session: PlayerSession, post: Post): Promise<boolean> {
+        // Access Control
+    }
+
+    async hideComment(session: PlayerSession, comment: Comment): Promise<boolean> {
+        // 
+    }
+    
+    // Edit
+
+    // Remove
+
+    addAdministrator(player: Player): boolean {
+
+    }
+
+    static postsOverlap(): boolean {
+
+    }
+
+    
+}
