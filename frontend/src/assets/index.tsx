51bddd0a9c788d36a3340c019827fd652a1e8830
Bob Joe <vzhan@Bobs-MacBook-Pro.local>
2022-04-25 23:05:56 -0400

    adding untracked files

---
+++ frontend/src/assets/index.tsx
@@ -0,0 +1,9 @@
+import bottle from './bottle.png';
+import tomb from './tomb.png';
+import post from './post.png';
+import chest from './chest.png';
+import flower from './flower.png';
+import fish from './fish.png';
+import ballon from './ballon.png';
+
+export {bottle, tomb, post, chest, flower, fish, ballon};
